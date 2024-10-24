48150f398fc156972c7ca97faf0e0a6b75ec184c
Dmytro Bezukh <dmytro.bezukh@crowdin.ua>
2024-10-24 16:22:56 +0300

    Add more AI providers. Add check command. Put all code in single prompt

---
+++ src/check.js
@@ -0,0 +1,384 @@
+//@ts-check
+import chalk from 'chalk';
+import cliWidth from 'cli-width';
+import fs from 'fs';
+import { Parser } from 'json2csv';
+import ora from 'ora';
+import { table } from 'table';
+import {
+    getCrowdin,
+    getUserId,
+    getCrowdinStrings,
+    validateAiProviderFields,
+    getTokenizer,
+    getPrompt,
+    getStringsChunks,
+    getModelLimits, getAiClient, stringifyStrings
+} from './utils.js';
+import {generateText, tool} from "ai";
+import {z} from 'zod';
+
+// tools that are used in the AI model. this way we get more predictable results from the model
+const AI_TOOLS = [{
+    type: "function",
+    function: {
+        name: "setErrors",
+        description: "Always use this function to set errors for strings.",
+        parameters: {
+            type: "object",
+            properties: {
+                errors: {
+                    type: "array",
+                    items: {
+                        type: "object",
+                        properties: {
+                            id: {
+                                type: "number",
+                                description: "This is the ID of the string that you are providing errors for."
+                            },
+                            error: {
+                                type: "string",
+                                description: "This is the error that you are providing for the string."
+                            }
+                        },
+                        required: ["id", "error"]
+                    },
+                }
+            }
+        }
+    }
+}];
+
+const DEFAULT_PROMPT = `Please analyse each string text and context.
+
+Use setErrors tool to add error to a string.
+
+For each string check if string text can be translated with high quality using provided context. Add error to a string only if:
+- context was not provided and string text can have different translations on target language;
+- context was provided but it's not enough to provide high quality translation for target language.
+Don't add error to a string if it can be translated unequivocally or when provided context is sufficient for accurate translation.
+Describe possible errors for each project target language. Skip languages without errors.
+
+Project target languages: %targetLanguages%.
+
+Strings (serialized as JSON):
+%strings%
+`;
+
+const spinner = ora();
+
+/**
+ * Prints the strings that would be updated in a dry run
+ * 
+ * @param {Array<object>} strings
+ */
+function dryRunPrint(strings) {
+    const stringsWithErrors = strings.filter((string) => string.errors);
+
+    const terminalWidth = cliWidth();
+
+    // Calculate the width for each column
+    const idColumnWidth = Math.ceil(terminalWidth * 0.13);
+    const textColumnWidth = Math.ceil(terminalWidth * 0.26);
+    const contextColumnWidth = Math.ceil(terminalWidth * 0.26);
+    const errorColumnWidth = Math.ceil(terminalWidth * 0.26);
+
+    const config = {
+        header: {
+            alignment: 'center',
+            content: 'Strings with errors'
+        },
+        columns: [
+            {
+                width: idColumnWidth,
+                wrapWord: true
+            },
+            {
+                width: textColumnWidth,
+                wrapWord: true
+            },
+            {
+                width: contextColumnWidth,
+                wrapWord: true
+            },
+            {
+                width: errorColumnWidth,
+                wrapWord: true
+            }
+        ]
+    };
+
+    let data = [];
+    for (const string of stringsWithErrors) {
+        data.push([string.identifier, string.text, string.context, string.errors.join('\n')]);
+    }
+
+    if (data.length < 1) {
+        console.log(`\nNo strings with insufficient context found.\n`);
+        return;
+    }
+
+    console.log('\n');
+    //@ts-ignore
+    console.log(table(data, config));
+
+    console.log(`\n${stringsWithErrors.length} strings have context errors. Please be aware that an LLM model may return different results for the same input next time you run the tool.\n`);
+}
+
+/**
+ * Writes the strings with AI context to a CSV file
+ * 
+ * @param {object} options 
+ * @param {Array<object>} strings
+ */
+function writeCsv(options, strings) {
+    const csvFile = options.csvFile;
+
+    const stringsWithErrors = strings.filter((string) => string.errors);
+
+    const data = stringsWithErrors.map((string) => {
+        return {
+            id: string.id,
+            key: string.identifier,
+            text: string.text,
+            context: string.context,
+            errors: string.errors.join('\n'),
+        };
+    });
+
+    if (data.length < 1) {
+        console.log(`\nNo strings with insufficient context found.\n`);
+        return;
+    }
+
+    try {
+        const parser = new Parser({ fields: ['id', 'key', 'text', 'context', 'errors'] });
+        const csv = parser.parse(data);
+
+        fs.writeFileSync(csvFile, csv);
+        console.log(`\n${data.length} strings saved to ${chalk.green(csvFile)}\n`);
+    } catch (err) {
+        console.error(`Error writing CSV file: ${err}`);
+    }
+}
+
+/**
+ * @param {Array<object>} strings 
+ * @param {object} [checkResults]
+ */
+async function appendCheckResults(strings, checkResults) {
+    for (const result of checkResults?.errors || []) {
+        const string = strings.find((s) => s.id === result.id);
+
+        if (string && result?.error) {
+            if (!string.errors) {
+                string.errors = [];
+            }
+
+            string.errors.push(result?.error);
+        }
+    }
+}
+
+/**
+ * Chunks the strings and code into smaller parts if needed and sends them to the AI model
+ * 
+ * @param {object} param0 
+ * @param {object} param0.apiClient
+ * @param {object} param0.options
+ * @param {Array<object>} param0.crowdinStrings
+ */
+async function checkStringsContext({ apiClient, options, crowdinStrings }) {
+    // if there are no strings left after screening, we return an empty context
+    if (!crowdinStrings.length) {
+        console.log(`${chalk.gray('  No strings found.')}`)
+        return { errors: [] };
+    }
+
+    const project = (await apiClient.projectsGroupsApi.getProject(options.project)).data;
+    const languages = (await apiClient.languagesApi.withFetchAll().listSupportedLanguages()).data;
+    const targetLanguageNames = project.targetLanguageIds.map(id => languages.find(({ data }) => data.id === id)?.data?.name).filter(a => !!a);
+
+    const tokenizer = getTokenizer(options.ai, options.model);
+    const prompt = getPrompt({ options, defaultPrompt: DEFAULT_PROMPT });
+
+    const modeLimits = getModelLimits(options);
+    const stringsChunkLimit = modeLimits.output / 4; // we assume that context will be longer than strings
+
+    const stringsChunks = getStringsChunks({
+        crowdinStrings,
+        tokenizer,
+        chunkLimit: stringsChunkLimit
+    });
+
+    let chunkNumber = 1;
+    let errors = [];
+
+    for (const stringsChunk of stringsChunks) {
+        spinner.start(`Processing chunk ${chunkNumber} of ${stringsChunks.length}`);
+
+        try {
+            const messages = buildMessages({ prompt, strings: stringsChunk, targetLanguageNames });
+
+            const response = await executePrompt({
+                apiClient,
+                messages,
+                options,
+            });
+
+            errors.push(...(response?.errors || []));
+            spinner.succeed();
+        } catch (e) {
+            spinner.fail();
+            console.log(`\n${e?.response?.data?.error?.message || e}`);
+        }
+
+        chunkNumber++;
+    }
+
+    return { errors };
+}
+
+/**
+ * Builds the chat messages for the AI model
+ * 
+ * @param {object} param0 
+ * @param {object} param0.prompt
+ * @param {object} param0.strings
+ */
+function buildMessages({ prompt, strings, targetLanguageNames }) {
+    const builtPrompt = prompt.replace('%strings%', stringifyStrings({ strings })).replace('%targetLanguages%', targetLanguageNames.join(', '));
+
+    return [
+        {
+            role: 'system',
+            content: 'You are helpful translator\'s assistant. You should help professional translator to find strings that are hard to translate because they can have different translations in target language without context.',
+        },
+        {
+            role: 'user',
+            content: builtPrompt,
+        }
+    ];
+}
+
+/**
+ * Picks a preferred AI provider and executes the prompt
+ * Returns an array of objects, every object is a string id and extracted context
+ * 
+ * @param {object} param0 
+ * @param {object} param0.apiClient
+ * @param {object} param0.options
+ * @param {Array<object>} param0.messages
+ */
+async function executePrompt({ apiClient, options, messages }) {
+    if (options.ai === 'crowdin') {
+        let aiResponse;
+        if (apiClient.aiApi.organization) {
+            aiResponse = (await apiClient.aiApi.createAiOrganizationProxyChatCompletion(options.crowdinAiId, {
+                model: options.model,
+                messages,
+                tools: AI_TOOLS
+            }));
+        } else {
+            aiResponse = (await apiClient.aiApi.createAiUserProxyChatCompletion(await getUserId(apiClient), options.crowdinAiId, {
+                model: options.model,
+                messages,
+                tools: AI_TOOLS
+            }));
+        }
+
+        const errors = [];
+        (aiResponse?.data?.choices?.[0]?.message?.tool_calls || []).forEach(toolCall => {
+            const args = toolCall?.function?.arguments;
+            errors.push(...(args ? JSON.parse(args) : []));
+        })
+
+        return { errors };
+    }
+
+    let client;
+    try {
+        client = getAiClient(options);
+    } catch(e) {
+        console.error('\n\nInvalid AI provider');
+        console.error(e);
+        process.exit(1);
+    }
+
+    const result = await generateText({
+        model: client(options.ai === 'azure' ? options.azureDeploymentName : options.model),
+        tools: {
+            setErrors: tool({
+                description: 'Always use this function to set errors for strings.',
+                parameters: z.object({
+                    errors: z.array(
+                      z.object({
+                          id: z.number().describe('This is the ID of the string that you are providing errors for.'),
+                          error: z.string().describe('This is the error that you are providing for the string.'),
+                      })
+                    ).describe('Array of errors to set'),
+                }),
+            }),
+        },
+        system: messages[0].content,
+        messages: [messages[1]],
+    });
+
+    let errors = [];
+
+    (result?.toolCalls || []).forEach(toolCall => {
+      errors.push(
+        ...toolCall.args.errors,
+      );
+    })
+
+    return { errors };
+}
+
+// main function that orchestrates the context check process
+async function check(_name, commandOptions, _command) {
+    try {
+        const options = commandOptions.opts();
+
+        if (!['terminal', 'csv'].includes(options.output)) {
+            console.error('Wrong value provided for --output option. terminal, csv and crowdin values are available.');
+            process.exit();
+        }
+
+        validateAiProviderFields(options);
+
+        const apiClient = await getCrowdin(options);
+
+        let strings = await getCrowdinStrings({ options, apiClient, spinner });
+
+        let checkResults = {};
+
+        try {
+            checkResults = await checkStringsContext({
+                apiClient,
+                crowdinStrings: strings,
+                options,
+            });
+        } catch (e) {
+            console.log('\nError during context check');
+            console.error(e);
+        }
+
+        try {
+            await appendCheckResults(strings, checkResults);
+        } catch (error) {
+            console.log('\nError during context check');
+            console.error(error);
+        }
+
+        if (options.output === 'csv') {
+            writeCsv(options, strings);
+        } else {
+            dryRunPrint(strings);
+        }
+    } catch (error) {
+        console.error('error:', error);
+    }
+}
+
+export default check;
--- src/configure.js
+++ src/configure.js
@@ -3,6 +3,7 @@ import inquirer from 'inquirer';
 import { getCrowdin, getUserId } from './utils.js';
 import chalk from 'chalk';
 import axios from 'axios';
+import {GoogleAuth} from "google-auth-library";
 
 async function configureCli(_name, commandOptions, _command) {
     const options = commandOptions.opts();
@@ -49,7 +50,14 @@ async function configureCli(_name, commandOptions, _command) {
         type: 'list',
         name: 'ai',
         message: 'AI provider:',
-        choices: [{ name: 'OpenAI', value: 'openai' }, { name: 'Crowdin AI Provider', value: 'crowdin' }],
+        choices: [
+          { name: 'Crowdin AI Provider', value: 'crowdin' },
+          { name: 'OpenAI', value: 'openai' },
+          { name: 'Google Gemini (Vertex AI API)', value: 'google-vertex' },
+          { name: 'MS Azure OpenAI', value: 'azure' },
+          { name: 'Anthropic', value: 'anthropic' },
+          { name: 'Mistral', value: 'mistral' },
+        ],
     }, {
         type: 'list',
         name: 'crowdin_ai_id',
@@ -75,13 +83,58 @@ async function configureCli(_name, commandOptions, _command) {
     }, {
         type: 'input',
         name: 'openai_key',
-        message: 'OpenAI key:',
+        message: 'OpenAI API key:',
         when: (answers) => answers.ai === 'openai' && !options.openAiKey,
+    }, {
+        type: 'input',
+        name: 'google_vertex_project',
+        message: 'Google Cloud project ID:',
+        when: (answers) => answers.ai === 'google-vertex' && !options.googleVertexProject,
+    }, {
+        type: 'input',
+        name: 'google_vertex_location',
+        message: 'Google Cloud project location:',
+        when: (answers) => answers.ai === 'google-vertex' && !options.googleVertexLocation,
+    }, {
+        type: 'input',
+        name: 'google_vertex_client_email',
+        message: 'Google Cloud service account client email:',
+        when: (answers) => answers.ai === 'google-vertex' && !options.googleVertexClientEmail,
+    }, {
+        type: 'input',
+        name: 'google_vertex_private_key',
+        message: 'Google Cloud service account private key:',
+        when: (answers) => answers.ai === 'google-vertex' && !options.googleVertexPrivateKey,
+    }, {
+        type: 'input',
+        name: 'azure_resource_name',
+        message: 'MS Azure OpenAI resource name:',
+        when: (answers) => answers.ai === 'azure' && !options.azureResourceName,
+    }, {
+        type: 'input',
+        name: 'azure_api_key',
+        message: 'MS Azure OpenAI API key:',
+        when: (answers) => answers.ai === 'azure' && !options.azureApiKey,
+    }, {
+        type: 'input',
+        name: 'azure_deployment_name',
+        message: 'MS Azure OpenAI deployment name:',
+        when: (answers) => answers.ai === 'azure' && !options.azureDeploymentName,
+    }, {
+        type: 'input',
+        name: 'anthropic_api_key',
+        message: 'Anthropic API key:',
+        when: (answers) => answers.ai === 'anthropic' && !options.anthropicApiKey,
+    }, {
+        type: 'input',
+        name: 'mistral_api_key',
+        message: 'Mistral API key:',
+        when: (answers) => answers.ai === 'mistral' && !options.mistralApiKey,
     }, {
         type: 'list',
         name: 'model',
-        message: 'AI model (gpt-4o or newer required):',
-        default: 'gpt-4o',
+        message: 'AI model (newest models with largest context window are preferred):',
+        when: answers => answers.ai !== 'azure',
         choices: async (answers) => {
             if (answers.ai === 'crowdin') {
                 const apiClient = await getCrowdin({ token: answers.token || options.token, url: answers.url || options.url });
@@ -100,7 +153,9 @@ async function configureCli(_name, commandOptions, _command) {
                 }
 
                 return models.map(model => { return { name: model.id, value: model.id } });
-            } else {
+            }
+
+            if (answers.ai === 'openai') {
                 try {
                     const openAiModels = (await axios.get('https://api.openai.com/v1/models', {
                         headers: {
@@ -114,7 +169,87 @@ async function configureCli(_name, commandOptions, _command) {
                     process.exit(1);
                 }
             }
+
+            if (answers.ai === 'mistral') {
+                return [
+                  'mistral-large-latest',
+                  'ministral-8b-latest',
+                  'ministral-3b-latest',
+                  'mistral-small-latest',
+                ].map(model => ({
+                    value: model,
+                    label: model,
+                }));
+            }
+
+            if (answers.ai === 'anthropic') {
+                return [
+                  'claude-3-5-sonnet-20240620',
+                  'claude-3-opus-20240229',
+                  'claude-3-sonnet-20240229',
+                  'claude-3-haiku-20240307',
+                ].map(model => ({
+                    value: model,
+                    label: model,
+                }));
+            }
+
+            if (answers.ai === 'google-vertex') {
+                try {
+                    const location = process.env.GOOGLE_VERTEX_LOCATION || answers.google_vertex_location;
+                    const project = process.env.GOOGLE_VERTEX_PROJECT || answers.google_vertex_project;
+
+                    const auth = new GoogleAuth({
+                        scopes: "https://www.googleapis.com/auth/cloud-platform",
+                        credentials: {
+                            private_key: process.env.GOOGLE_VERTEX_PRIVATE_KEY || answers.google_vertex_private_key.replace(/\\n/g, '\n'),
+                            client_email: process.env.GOOGLE_VERTEX_CLIENT_EMAIL || answers.google_vertex_client_email,
+                        }
+                    });
+                    const token = await auth.getAccessToken();
+
+                    const url = `https://${location}-aiplatform.googleapis.com/v1/projects/${project}/locations/${location}/models`;
+                    const models = (await axios.get(url, {
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}`,
+                        }
+                    }))?.data?.models || [];
+
+                    return [
+                      ...([
+                        'gemini-1.5-pro',
+                        'gemini-1.5-pro-002',
+                        'gemini-1.5-pro-001',
+                        'gemini-1.5-flash',
+                        'gemini-1.5-flash-001',
+                        'gemini-1.5-flash-002',
+                      ]).map(model => ({
+                          value: model,
+                          label: model,
+                      })),
+                      ...models.map(model => ({
+                         value: model.displayName,
+                         label: model.displayName,
+                      })),
+                    ];
+                } catch (e) {
+                    console.error(`Error: ${e.message}`);
+                    process.exit(1);
+                }
+            }
         },
+    }, {
+        type: 'input',
+        name: 'contextWindowSize',
+        message: 'Model context window size in tokens:',
+        default: '128000',
+    }, {
+        type: 'input',
+        name: 'maxOutputTokens',
+        message: 'Model maximum output tokens count:',
+        default: '16384',
     }, {
         type: 'list',
         name: 'screen',
@@ -170,12 +305,23 @@ async function configureCli(_name, commandOptions, _command) {
         chalk.yellow('--project=') + chalk.white(`${answers.project} `) +
         chalk.yellow('--ai=') + chalk.white(`"${answers.ai}" `) +
         (answers.openai_key && !options.openAiKey ? chalk.yellow('--openAiKey=') + chalk.white(`"${answers.openai_key}" `) : '') +
+        (answers.google_vertex_project && !options.googleVertexProject ? chalk.yellow('--googleVertexProject=') + chalk.white(`"${answers.google_vertex_project}" `) : '') +
+        (answers.google_vertex_location && !options.googleVertexLocation ? chalk.yellow('--googleVertexLocation=') + chalk.white(`"${answers.google_vertex_location}" `) : '') +
+        (answers.google_vertex_client_email && !options.googleVertexClientEmail ? chalk.yellow('--googleVertexClientEmail=') + chalk.white(`"${answers.google_vertex_client_email}" `) : '') +
+        (answers.google_vertex_private_key && !options.googleVertexPrivateKey ? chalk.yellow('--googleVertexPrivateKey=') + chalk.white(`"${answers.google_vertex_private_key}" `) : '') +
         (answers.crowdin_ai_id ? chalk.yellow('--crowdinAiId=') + chalk.white(`${answers.crowdin_ai_id} `) : '') +
-        chalk.yellow('--model=') + chalk.white(`"${answers.model}" `) +
+        (answers.azure_resource_name && !options.azureResourceName ? chalk.yellow('--azureResourceName=') + chalk.white(`"${answers.azure_resource_name}" `) : '') +
+        (answers.azure_api_key && !options.azureApiKey ? chalk.yellow('--azureApiKey=') + chalk.white(`"${answers.azure_api_key}" `) : '') +
+        (answers.azure_deployment_name && !options.azureDeploymentName ? chalk.yellow('--azureDeploymentName=') + chalk.white(`"${answers.azure_deployment_name}" `) : '') +
+        (answers.anthropic_api_key && !options.anthropicApiKey ? chalk.yellow('--anthropicApiKey=') + chalk.white(`"${answers.anthropic_api_key}" `) : '') +
+        (answers.mistral_api_key && !options.mistralApiKey ? chalk.yellow('--mistralApiKey=') + chalk.white(`"${answers.mistral_api_key}" `) : '') +
+        (answers.ai !== 'azure' ? chalk.yellow('--model=') + chalk.white(`"${answers.model}" `) : '') +
         (answers.promptFile ? chalk.yellow('--promptFile=') + chalk.white(`"${answers.promptFile}" `) : '') +
         chalk.yellow('--localFiles=') + chalk.white(`"${answers.localFiles}" `) +
         chalk.yellow('--localIgnore=') + chalk.white(`"${answers.localIgnore}" `) +
         chalk.yellow('--crowdinFiles=') + chalk.white(`"${answers.crowdinFiles}" `) +
+        chalk.yellow('--contextWindowSize=') + chalk.white(`"${answers.contextWindowSize}" `) +
+        chalk.yellow('--maxOutputTokens=') + chalk.white(`"${answers.maxOutputTokens}" `) +
         (answers.screen !== 'none' ? chalk.yellow('--screen=') + chalk.white(`"${answers.screen}" `) : '') +
         (answers.croql.length > 0 ? chalk.yellow('--croql=') + chalk.white(`"${answers.croql.replaceAll('"', '\\\"')}" `) : '') +
         chalk.yellow('--output=') + chalk.white(`"${answers.output}" `) +
